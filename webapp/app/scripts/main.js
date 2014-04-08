ca623e592b6e3b47b2f064952cf3df91c32119a3
Magnus Krane <magnus.krane@gmail.com>
2014-04-08 16:31:13 +0200

    All stations are in correct area-responsible

--- webapp/app/scripts/main.js
+++ webapp/app/scripts/main.js
@@ -131,6 +131,17 @@ $(document).ready(function() {
         weight: 1,
         opacity: 1,
     };
+    
+    var popup = L.popup();
+
+    function onMapClick(e) {
+        popup
+            .setLatLng(e.latlng)
+            .setContent("You clicked the map at " + e.latlng.toString())
+            .openOn(map);
+    }
+
+    map.on('click', onMapClick);
 
     
 });
