203c4bdf99e450ea4710627291baf9ab55ad13ea
Magnus Krane <magnus.krane@gmail.com>
2014-04-25 22:16:54 +0200

    trying html in divicon

--- webapp/app/scripts/main.js
+++ webapp/app/scripts/main.js
@@ -276,7 +276,9 @@ $(document).ready(function() {
                     var popupContent = feature.properties.tags.name;
                     //return generatePieChartForCluster(latlng);
                     coordinates.push(latlng);
-                    var htmlIcon = L.divIcon({ iconSize: new L.Point(50, 50), html: 'foo bar sdfg nmdfg fnæ gdf gklsfdg nædfsg nkdfsg jdfsg <img src="../images/marker-icon.png" alt="" align="right">' });
+                    var htmlIcon = L.divIcon({ iconSize: new L.Point(50, 50), html: '<textarea rows="4" cols="50">At w3schools.com you will learn how to make a website. ' +
+                                             'We offer free tutorials in all web development technologies. ' +
+                                             '</textarea> <img src="../images/marker-icon.png" alt="" align="right">' });
                     return L.marker(latlng, {icon: htmlIcon}).bindPopup(popupContent);
                     //return L.divIcon({ iconSize: new L.Point(50, 50), html: 'foo bar' });
                 }
