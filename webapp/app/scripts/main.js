9774bdbf7e588ffba193a0b81ac013ff2acc41ad
Magnus Krane <magnus.krane@gmail.com>
2014-04-26 14:55:59 +0200

    trying html in divicon

--- webapp/app/scripts/main.js
+++ webapp/app/scripts/main.js
@@ -278,7 +278,7 @@ $(document).ready(function() {
                     //return generatePieChartForCluster(latlng);
                     coordinates.push(latlng);
                     var htmlIcon = L.divIcon({ classname: 'info', iconSize: new L.Point(50, 50), html: popupContent });
-                    L.marker(new L.latLng(latlng.lat,latlng.lng-(2-(map.getZoom()*0.1))), {icon: htmlIcon}).bindPopup(popupContent).addTo(railStationsInfoBoxes);
+                    L.marker(new L.latLng(latlng.lat,latlng.lng-(2-(map.getZoom()*0.25))), {icon: htmlIcon}).bindPopup(popupContent).addTo(railStationsInfoBoxes);
                     return L.marker(latlng).bindPopup(popupContent);
                 }
             }).addTo(railStations);
