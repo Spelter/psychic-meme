68abbbfdbf998636cbf9c871700288eb1dbcd344
Magnus Krane <magnus.krane@gmail.com>
2014-04-26 15:19:14 +0200

    trying html in divicon

--- webapp/app/scripts/main.js
+++ webapp/app/scripts/main.js
@@ -282,7 +282,7 @@ $(document).ready(function() {
                     return L.marker(latlng).bindPopup(popupContent);
                 }
             }).addTo(railStations);
-            console.log(map.getZoom()%8);
+            console.log(map.getZoom()%12);
             map.fitBounds(new L.latLngBounds(coordinates).pad(0.2));
         });
     };
