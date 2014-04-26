b34efebde42a24527c44ad02b3a49a724c8f5625
Magnus Krane <magnus.krane@gmail.com>
2014-04-26 12:22:58 +0200

    trying html in divicon

--- webapp/app/scripts/main.js
+++ webapp/app/scripts/main.js
@@ -272,7 +272,7 @@ $(document).ready(function() {
         $.getJSON(host + '/rail/view/' + searchName)
             .done(function(data) {
             //console.log(data);
-            console.log(map.getZoom()/0.2);
+            console.log(map.getZoom()*0.2);
             L.geoJson(data, {
                 pointToLayer: function (feature, latlng) {
                     //var popupOptions = {maxWidth: 20};
