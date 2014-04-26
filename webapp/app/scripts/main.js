3dbf328cb23995134442a43182d93652bf2d13db
Magnus Krane <magnus.krane@gmail.com>
2014-04-26 15:09:37 +0200

    trying html in divicon

--- webapp/app/scripts/main.js
+++ webapp/app/scripts/main.js
@@ -279,7 +279,7 @@ $(document).ready(function() {
                     coordinates.push(latlng);
                     var htmlIcon = L.divIcon({ classname: 'info', iconSize: new L.Point(50, 50), html: popupContent });
                     L.marker(new L.latLng(latlng.lat,latlng.lng-((map.getZoom()*0.25))), {icon: htmlIcon}).bindPopup(popupContent).addTo(railStationsInfoBoxes);
-                    console.log(map.getZoom()*0.2);
+                    console.log(map.getZoom()*0.1);
                     return L.marker(latlng).bindPopup(popupContent);
                 }
             }).addTo(railStations);
