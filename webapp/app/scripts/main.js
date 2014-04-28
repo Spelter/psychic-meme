ef80c16a87634d64a2284206a926b109045a6e0b
Magnus Krane <magnus.krane@gmail.com>
2014-04-28 17:19:33 +0200

    trying html in divicon

--- webapp/app/scripts/main.js
+++ webapp/app/scripts/main.js
@@ -289,8 +289,8 @@ $(document).ready(function() {
                     layer.setLatLng(new L.latLng(latlng.lat,latlng.lng-((7-(map.getZoom()*0.7)))));
                 }
             });
-                L.Util.requestAnimFrame(map.invalidateSize, map, false, map._container);
         });
+        L.Util.requestAnimFrame(map.invalidateSize, map, false, map._container);
     };
 
 });
