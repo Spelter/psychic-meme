99faff3f4a5cff947a8f32fea59b3277d095cb67
Magnus Krane <magnus.krane@gmail.com>
2014-04-29 16:44:00 +0200

    Div icon and button.
    Changed marker to a div icon with markin picture and textbox for info.
    Added a button in bottom left corner, shall eventually be time control.

--- webapp/app/scripts/main.js
+++ webapp/app/scripts/main.js
@@ -275,16 +275,143 @@ $(document).ready(function() {
                 pointToLayer: function (feature, latlng) {
                     var popupContent = feature.properties.tags.name;
                     coordinates.push(latlng);
-                    return L.marker(latlng).bindPopup(popupContent);
+                    var htmlIcon = L.divIcon({ className: 'iconbox', iconSize: new L.Point(50, 50), html: '<div class="info"> test</div>' });
+                    //L.marker(latlng, {icon: htmlIcon}).addTo(railStationsInfoBoxes);
+                    return L.marker(latlng, {icon: htmlIcon}).bindPopup(popupContent);
+                    //return L.marker(latlng).bindPopup(popupContent);
                 }
             }).addTo(railStations);
             console.log(map.getZoom());
-            //map.fitBounds(new L.latLngBounds(coordinates).pad(0.2));
-            map.setZoom(4);
-            for (var i = 0; i < coordinates.length; i++) {
-                var htmlIcon = L.divIcon({ className: 'iconbox', iconSize: new L.Point(50, 50), html: 'test' });
-                L.marker(new L.latLng(coordinates[i].lat, coordinates[i].lng-((3-(map.getZoom()*0.2)))), {icon: htmlIcon}).addTo(railStationsInfoBoxes);
-            };
+            map.fitBounds(new L.latLngBounds(coordinates).pad(0.2));
         });
     };
+
+    L.Control.Button = L.Control.extend({
+      options: {
+        position: 'bottomleft'
+      },
+      initialize: function (options) {
+        this._button = {};
+        this.setButton(options);
+      },
+     
+      onAdd: function (map) {
+        this._map = map;
+        var container = L.DomUtil.create('div', 'info');
+        
+        this._container = container;
+        
+        this._update();
+        return this._container;
+      },
+     
+      onRemove: function (map) {
+      },
+     
+      setButton: function (options) {
+        var button = {
+          'text': options.text,                 //string
+          'iconUrl': options.iconUrl,           //string
+          'onClick': options.onClick,           //callback function
+          'hideText': !!options.hideText,         //forced bool
+          'maxWidth': options.maxWidth || 70,     //number
+          'doToggle': options.toggle,           //bool
+          'toggleStatus': false                 //bool
+        };
+     
+        this._button = button;
+        this._update();
+      },
+      
+      getText: function () {
+        return this._button.text;
+      },
+      
+      getIconUrl: function () {
+        return this._button.iconUrl;
+      },
+      
+      destroy: function () {
+        this._button = {};
+        this._update();
+      },
+      
+      toggle: function (e) {
+        if(typeof e === 'boolean'){
+            this._button.toggleStatus = e;
+        }
+        else{
+            this._button.toggleStatus = !this._button.toggleStatus;
+        }
+        this._update();
+      },
+      
+      _update: function () {
+        if (!this._map) {
+          return;
+        }
+     
+        this._container.innerHTML = '';
+        this._makeButton(this._button);
+     
+      },
+     
+      _makeButton: function (button) {
+        var newButton = L.DomUtil.create('div', 'leaflet-buttons-control-button', this._container);
+        if(button.toggleStatus)
+            L.DomUtil.addClass(newButton,'leaflet-buttons-control-toggleon');
+            
+        var image = L.DomUtil.create('img', 'leaflet-buttons-control-img', newButton);
+        image.setAttribute('src',button.iconUrl);
+        
+        if(button.text !== ''){
+     
+          L.DomUtil.create('br','',newButton);  //there must be a better way
+     
+          var span = L.DomUtil.create('span', 'leaflet-buttons-control-text', newButton);
+          var text = document.createTextNode(button.text);  //is there an L.DomUtil for this?
+          span.appendChild(text);
+          if(button.hideText)
+            L.DomUtil.addClass(span,'leaflet-buttons-control-text-hide');
+        }
+     
+        L.DomEvent
+          .addListener(newButton, 'click', L.DomEvent.stop)
+          .addListener(newButton, 'click', button.onClick,this)
+          .addListener(newButton, 'click', this._clicked,this);
+        L.DomEvent.disableClickPropagation(newButton);
+        return newButton;
+     
+      },
+      
+      _clicked: function () {  //'this' refers to button
+        if(this._button.doToggle){
+            if(this._button.toggleStatus) { //currently true, remove class
+                L.DomUtil.removeClass(this._container.childNodes[0],'leaflet-buttons-control-toggleon');
+            }
+            else{
+                L.DomUtil.addClass(this._container.childNodes[0],'leaflet-buttons-control-toggleon');
+            }
+            this.toggle();
+        }
+        return;
+      }
+     
+    });
+
+    var myButtonOptions = {
+          'text': 'test',  // string
+          'iconUrl': 'images/marker-icon.png',  // string
+          'onClick': my_button_onClick,  // callback function
+          'hideText': true,  // bool
+          'maxWidth': 30,  // number
+          'doToggle': false,  // bool
+          'toggleStatus': false  // bool
+    }   
+
+    var myButton = new L.Control.Button(myButtonOptions).addTo(map);
+
+    function my_button_onClick() {
+        console.log("someone clicked my button");
+    }
 });
