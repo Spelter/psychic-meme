bc9501743fd882fb5227a313f183097a4a68e2ab
Magnus Krane <magnus.krane@gmail.com>
2014-05-09 00:43:21 +0200

    test db

--- webapp/app.js
+++ webapp/app.js
@@ -350,11 +350,6 @@ function fetchSeveralStationsFromDatabase (stretch) {
 	      rows.push(row);
 	    });
 	    console.log(queryString);
-	    query.on('end', function(result) {
-		  //fired once and only once, after the last row has been returned and after all 'row' events are emitted
-		  //in this example, the 'rows' array now contains an ordered set of all the rows which we received from postgres
-		  console.log(result.rowCount + ' rows were received');
-		})
 		return rows;
 	});
 }
